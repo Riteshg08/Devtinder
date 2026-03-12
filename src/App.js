4d46fdd7ef9adfcd8afc580e8489ffd0bae2f176
Ritesh Gurkhe <riteshgurkhe@gmail.com>
2026-03-12 22:10:54 +0530

    Exploring different routing expression

--- src/App.js
+++ src/App.js
@@ -4,45 +4,64 @@ const express = require('express');
 // app is used to define routes, middleware(code that runs between the request and response), and server config
 const app = express();
 
-// app.use("/test/result",(req,res) => {
-//     res.send("Test result will be out as soon as possible!");
-// });
-
-// app.use("/test",(req,res)=>{
-//     //send the response to the client and end the request
-//     res.send("Test page is fetch from the server!");
-// });
-
-// app.use("/",(req,res)=>{
-//     res.send("Dashboard is fetch from the server!");
-// });
-
-// Checking all the HTTP methods
-//Now if we run always this will run becoz order matters
-app.use("/user",(req,res)=>{
-    res.send("Fetching the data from this route!");
+//request handlers are the functions that are executed when a route is matched
+app.use((req, res) => {
+    res.send("Hello World");
 });
 
-// Checking only for the get method
-app.get("/user",(req,res) => {
-    res.send("Testing the get call!");
+//Different express routing patterns
+
+// After b we can add any character but end should be with c
+// eg- abbbbc, abdfghhjgc
+
+app.use("/ab*c", (req, res) => {
+    res.send("Matched ab*c");
 });
 
-app.post("/user",(req,res) => {
-    res.send("Pushing new content in database!");
+/*
++ => After b we can add any number of b
+app.use("/ab+c", (req, res) => {
+    res.send("Matched ab*c");
+});
+*/
+
+/*
+? => Here b is optional means if we do not write b then also it is fine
+app.use("/ab?c", (req, res) => {
+    res.send("Matched ab*c");
+});
+*/
+
+// Routes Parameters
+// Means for each route we can assign unique id
+app.get("/user/:id", (req, res) => {
+    res.send(req.params.id);
 });
 
-app.delete("/user",(req,res) => {
-    res.send("Deleting the content from the database!");
+app.get("/user/:name", (req, res) => {
+    res.send(req.params.name);
 });
 
-app.put("/user",(req,res) => {
-    res.send("Deleting the specific field from the database!");
+//Assigning multiple routes parameter to single route
+app.get("/user/:id/post/:postId", (req, res) => {
+    res.json(req.params);
+    //it will give data in json format
 });
 
-app.patch("/user",(req,res) =>{
-    res.send("Deleting the data or adding new data to the database!")
-})
+//Regex route
+// Here we can add bc multiple times
+app.get(/a(bc)+d/, (req, res) => {
+    res.send("Regex matched");
+});
+
+
+//Query Parameters - Query parameters are extra data sent in the URL after ?
+//eg- /search?name=ritesh&id=48
+// where name and id are the key's and ritesh and 48 is are there corresponding values
+app.get("/search", (req, res) => {
+    res.send(req.query);
+});  
+
 
 app.listen(7777, () => {
     console.log("Server is created successfully!");
