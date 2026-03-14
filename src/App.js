e9f85eeef4813411a64e3923a6771e97172dc3b5
Ritesh Gurkhe <riteshgurkhe@gmail.com>
2026-03-14 11:35:55 +0530

    Playing with routes and intro to middelwears

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ const express = require('express');
 const app = express();
 
 //request handlers are the functions that are executed when a route is matched
-app.use((req, res) => {
-    res.send("Hello World");
+// app.use((req, res) => {
+//     res.send("Hello World");
+// });
+
+//this will only handel get request to /user route
+app.get("/user",(req,res) => {
+    res.send({first:"Ritesh",last:"Gurkhe"});
+});
+
+app.post("/user",(req,res) => {
+    //saving the data to the data base
+    res.send("Data is saved successfully");
 });
 
 //Different express routing patterns
@@ -60,9 +70,48 @@ app.get(/a(bc)+d/, (req, res) => {
 // where name and id are the key's and ritesh and 48 is are there corresponding values
 app.get("/search", (req, res) => {
     res.send(req.query);
-});  
+}); 
 
 
+//Multiple callback functions
+//1
+app.use("/game",(req,res,next) =>{
+    console.log("Game route is called");
+    // res.send("Welcome to the game 1");
+    next();
+},(req,res,next) =>{ 
+    console.log("Game route is called again");
+    res.send("Welcome to the game 2");
+    next();
+}); 
+
+//app.use("/route",rH,rH2,[rH3,rH4],rh5);
+//--------------------------------------------------------
+
+//2
+//here the 1st,2nd  and 3rd callback function is called as middleware and the 2nd callback function is called as request handler
+app.use("/gamer",(req,res,next) =>{
+    console.log("Gamer route is called");
+    // res.send("Welcome to the gamer");
+    next();
+});
+app.use("/gamer",(req,res,next) =>{
+    console.log("Gamer route is called again");
+    // res.send("Welcome to the gamer again");
+    next();
+});
+app.use("/gamer",(req,res,next) =>{
+    console.log("Gamer route is called again");
+    // res.send("Welcome to the gamer again");
+    next();
+});
+app.use("/gamer",(req,res) =>{
+    console.log("Gamer route is called again");
+    res.send("Welcome to the gamer again");
+});
+
+
+//It is used to start the server and listen for incoming requests on a specific port
 app.listen(7777, () => {
     console.log("Server is created successfully!");
 });
