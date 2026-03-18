822d3de9e56f72ee7d43a99bf9a82200e61bdc6d
Ritesh Gurkhe <riteshgurkhe@gmail.com>
2026-03-19 00:03:37 +0530

    Add cookie-parser and jsonwebtoken for authentication and session management

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const app = express();
 
 const User = require("./models/user");
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const { validateSignupData } = require("./utils/validator");
 
@@ -11,21 +13,26 @@ const { validateSignupData } = require("./utils/validator");
 //express.json() converts JSON request into js object so we can access it
 app.use(express.json());
 
+//It is an express middleware used to parse cookies from Http request
+//and make them accessible through req.cookies property in the route handlers
+app.use(cookieParser());
+
 //we use async because saving data in the database is asynchronous operation and we need to wait for it to complete before sending the response to the client
 app.post("/signup", async (req, res) => {
     //creating a new instance of the user model
     //pushing a dynamic data given by the client in the request body to the user model
 
     try {
-            const { firstName, lastName, email,age, gender, password, skills, about } = req.body;
+        const { firstName, lastName, email, age, gender, password, skills, about } = req.body;
 
         //Data validation
         validateSignupData(req);
 
         //password hashing
         //we can use bcrypt package for hashing the password
-        const passwordHash = await bcrypt.hash(password, 10);   
+        const passwordHash = await bcrypt.hash(password, 10);
         console.log(passwordHash);
+        //instead of passing req.body this is the better way
         const user = new User({
             firstName,
             lastName,
@@ -53,26 +60,57 @@ app.post("/signup", async (req, res) => {
 });
 
 //login API to authenticate the user 
-app.post("/login", async(req, res) => {
-    const { email, password} = req.body;
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
-    try{
-        const user = await User.findOne({email: email});
-        if(!user){
-            res.status(404).send("User not found!!");
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(404).send("User not found!!");
         }
 
-        const isPasswordMatch = await bcrypt.compare(password,user.password);
-        if(!isPasswordMatch){
-            res.status(404).send("Enter appropriate credentials!!");
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            throw new Error("Enter appropriate credentials!!");
         }
-        else{
+        else {
+            //creating a JWT token
+            const token = await jwt.sign({ userId: user._id }, "Devtinder@namastenode");
+            console.log(token);
+
+            // Add the token to cookie and send the response to the client
+            res.cookie("token", token);
             res.send("User is logged successfully!!");
         }
     }
-    catch(err){
-        res.status(400).send("Error: " +err.message);
+    catch (err) {
+        res.status(400).send("Error: " + err.message);
     }
+})
+
+app.get("/profile", async (req, res) => {
+    //get the token from the cookie
+    try {
+        const cookie = req.cookies;
+
+        const { token } = cookie;
+        if(!token){
+            throw new Error("Invalid token!!");
+        }
+
+        const decodedToken = await jwt.verify(token, "Devtinder@namastenode");
+
+        const user = await User.findById(decodedToken.userId);
+        if (!user) {
+           throw new Error("User not found!!");
+        } 
+        res.send(user);
+    }
+    catch (err) {
+        res.status(400).send("Error: " + err.message);
+    }
+
 })
 
 
