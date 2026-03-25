c0e637afe3df19722bba1feb7ae13d03bb8ef53e
Ritesh Gurkhe <riteshgurkhe@gmail.com>
2026-03-25 22:34:15 +0530

    Create redux store and subscibe navbar to it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+ 
+    const user = useSelector((store) => store.user);
+
     return (
         <div className="navbar bg-base-300 shadow-sm">
             <div className="flex-1">
                 <a className="btn btn-ghost text-xl">Devtinder</a>
             </div>
-            <div className="flex gap-2">
+            {user && <div className="flex items-center">
+                <p>Welcome, {user.firstName}</p>
                 <div className="dropdown dropdown-end mx-5">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
@@ -26,7 +32,7 @@ const Navbar = () => {
                         <li><a>Logout</a></li>
                     </ul>
                 </div>
-            </div>
+            </div>}
         </div>
     );
 };
